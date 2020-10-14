2643ad63b6cbbeabc61004d7fc2026e49ff4565c
Tepes Paula Ioana <oanapaulatepes88@gmail.com>
2020-10-14 20:40:23 +0300

    Add project files.

---
+++ TepesOana_Lab2/Data/LibraryContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TepesOana_Lab2.Models;
+
+namespace TepesOana_Lab2.Data
+{
+    public class LibraryContext:DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Customer>().ToTable("Order");
+            modelBuilder.Entity<Customer>().ToTable("Book");
+
+        }
+    }
+}
---
+++ TepesOana_Lab2/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace TepesOana_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public int BookID { get; set; }
+
+        public Customer Customer { get; set; }
+        public Book Book { get; set; }
+    }
+}
